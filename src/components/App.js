e3663821d06fdc914a3564a98a513ba970cd2572
Andrew Stahl <andrewstahl96@gmail.com>
2022-07-16 12:59:34 -0400

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import SushiContainer from "./SushiContainer";
 import Table from "./Table";
 
 const API = "http://localhost:3001/sushis";
 
 function App() {
+  const [tableVariables, setTableVariables] = useState({
+    moneyRemaining: 100,
+    plates: []
+  })
+  
+  function handleOnBuyClick(sushiPrice) {
+    setTableVariables({
+      moneyRemaining: tableVariables.moneyRemaining - sushiPrice,
+      plates: [...tableVariables.plates, sushiPrice]
+    })
+  }
+  
   return (
     <div className="app">
-      <SushiContainer />
-      <Table />
+      <SushiContainer 
+        API={API} 
+        onBuyClick={handleOnBuyClick} 
+        moneyRemaining={tableVariables.moneyRemaining}
+      />
+      <Table moneyRemaining={tableVariables.moneyRemaining} plates={tableVariables.plates}/>
     </div>
   );
 }
--- src/components/SushiContainer.js
+++ src/components/SushiContainer.js
@@ -1,11 +1,46 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Sushi from "./Sushi";
 import MoreButton from "./MoreButton";
 
-function SushiContainer(props) {
+function SushiContainer({ API, onBuyClick, moneyRemaining }) {
+  const [sushiList, setSushiList] = useState([]);
+  const [arrayElements, setArrayElements] = useState({
+    startingNumber: 0,
+    endingNumber: 4
+  });
+  const [sushiBought, setSushiBought] = useState(false);
+
+  useEffect(() => {
+    fetch(API)
+      .then(r => r.json())
+      .then(data => setSushiList(data))
+  }, []);
+
+  function handleMoreButtonClick() {
+    setArrayElements({
+      startingNumber: arrayElements.startingNumber + 4,
+      endingNumber: arrayElements.endingNumber + 4
+    })
+  }
+
+  const visibleListArray = sushiList
+    .slice(
+      arrayElements.startingNumber, 
+      arrayElements.endingNumber
+    )
+    .map(sushi => 
+      <Sushi 
+        sushi={sushi} 
+        sushiBought={sushiBought}
+        onBuyClick={onBuyClick} 
+        moneyRemaining={moneyRemaining} 
+      />
+    )
+  
   return (
     <div className="belt">
-      {/* Render Sushi components here! */}
-      <MoreButton />
+      {visibleListArray}
+      <MoreButton onMoreButtonClick={handleMoreButtonClick}/>
     </div>
   );
 }
